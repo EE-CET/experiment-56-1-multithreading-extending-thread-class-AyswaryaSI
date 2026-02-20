84d88684c0b50d18121fff4855fa6b596ba213ba
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-20 09:22:38 +0530

    Update ExtendThread.java

--- src/ExtendThread.java
+++ src/ExtendThread.java
@@ -1,11 +1,12 @@
-class MyThread extends Thread {
-    // TODO: Override the run() method
-    // Inside run(), print "Thread is running"
+class MyThread extends Thread{
+      public void run(){
+                 System.out.println("Thread is running");
+      } 
+      
+public class ExtendThread {
+        public static void main(String[]args)[
+                 MyThread t1 = new MyThread();
+                 t1.start();
+      }
 }
 
-public class ExtendThread {
-    public static void main(String[] args) {
-        // TODO: Create an object of MyThread
-        // TODO: Start the thread using start() method
-    }
-}
